414379eaa2334c50187d6d3b5e9868548a57e1bb
jb0001994 <jb0001994@aol.com>
2017-01-25 15:52:27 -0800

    Debugging for github pages

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
      entry: {
-        app: [  
-            './src/firebase-auth.js', 
-            './src/navbar-login', 
+        app: [
+            './src/firebase-auth.js',
+            './src/navbar-login',
             './src/new-post.js',
             './src/find.js',
-            './src/navbar-signup.js',
+            'src/navbar-signup.js',
             './src/detailed-view.js',
             './src/profile.js',
             './src/hub.js',
-            './src/firebase.js'          
+            './src/firebase.js'
         ]
      },
      output: {
